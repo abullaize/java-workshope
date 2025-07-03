752c71e065486efd13aa662ddf31065cf5c1637a
abullaize <muhammadabullaizea24cse@vetias.ac.in>
2025-07-03 14:01:43 +0530

    tempdata.java

---
+++ src/com/vetias/worksop/tempdata/beans/tempdata.java
@@ -0,0 +1,21 @@
+package com.vetias.worksop.tempdata.beans;
+
+public class tempdata {
+    public static void main(String[]agrs){
+        private String setName("VET thindal");
+        private double alatitude(10.12345);
+        private double alongitude(78.12345);
+        private String setAddress("thindal,erode,tamil nadu");
+        private String setPostalcode("1012345");
+        private String setContact("7812345");
+
+        System.out.println("Name:"+ setName);
+        System.out.println("latitude:"+ alatitude);
+        System.out.println("alongitude:"+ alongitude);
+        System.out.println("Address:"+ setAddress);
+        System.out.println("Postal code:"+ setPostalcode);
+        System.out.println("Contact:"+ sstContact);
+        
+
+    }
+}
